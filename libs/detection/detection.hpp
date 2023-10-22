73f8c49e210105ff7679afff8607fa16a2d3199d
Mayank Deshpande <deshpandemayank5@gmail.com>
2023-10-22 12:58:25 -0400

    Added Doxygen documentation.
    Commented detection.hpp and src.cpp in doxygen format

--- libs/detection/detection.hpp
+++ libs/detection/detection.hpp
@@ -4,16 +4,45 @@
 #include <opencv2/opencv.hpp>
 #include <iostream> // for input/output operations
 #include <vector>   // for using std::vector
+
+/**
+ * @class DetectionClass
+ * @brief A class for performing face detection using a deep learning model.
+ *
+ * This class initializes a face detection model and provides functions to 
+ * initialize a video stream from a camera and detect faces in the frames 
+ * obtained from the video stream.
+ */
 class DetectionClass {
 public:
+    /**
+     * @brief Constructor to initialize the DetectionClass object.
+     * @param modelPath Path to the pre-trained face detection model file.
+     * @param configPath Path to the configuration file for the model.
+     */
     DetectionClass(const std::string& modelPath, const std::string& configPath);
+
+    /**
+     * @brief Destructor to release resources used by DetectionClass.
+     */
     ~DetectionClass();
-    bool initVideoStream(int deviceID);  // Initialize video stream from camera
-    std::vector<cv::Rect> detectFaces(); // Detect faces in the current frame
+
+    /**
+     * @brief Initialize the video stream from the specified camera device.
+     * @param deviceID Identifier of the camera device (usually 0 for the default camera).
+     * @return True if the video stream is successfully opened, false otherwise.
+     */
+    bool initVideoStream(int deviceID);
+
+    /**
+     * @brief Detect faces in the current frame obtained from the video stream.
+     * @return A vector of cv::Rect representing the detected faces' bounding boxes.
+     */
+    std::vector<cv::Rect> detectFaces();
 
 private:
-    cv::dnn::Net faceDetectionModel;
-    cv::VideoCapture videoCapture;
+    cv::dnn::Net faceDetectionModel; ///< Deep learning face detection model.
+    cv::VideoCapture videoCapture;   ///< Video capture object for accessing frames from the camera.
 };
 
 #endif // DETECTION_HPP
--- libs/detection/src.cpp
+++ libs/detection/src.cpp
@@ -1,20 +1,37 @@
 #include "detection.hpp"
 
+/**
+ * @brief Constructor for the DetectionClass.
+ * @param modelPath Path to the pre-trained face detection model file.
+ * @param configPath Path to the configuration file for the model.
+ */
 DetectionClass::DetectionClass(const std::string& modelPath, const std::string& configPath) {
     // Constructor implementation (if any)
     // Initialize the face detection model with the provided paths
 }
 
+/**
+ * @brief Destructor for the DetectionClass.
+ */
 DetectionClass::~DetectionClass() {
     // Destructor implementation (if any)
 }
 
+/**
+ * @brief Initialize the video stream from the specified camera device.
+ * @param deviceID Identifier of the camera device (usually 0 for the default camera).
+ * @return True if the video stream is successfully opened, false otherwise.
+ */
 bool DetectionClass::initVideoStream(int deviceID) {
     // Function implementation
     // Initialize the video stream from the specified device (camera)
     // Return true if successfully opened, false otherwise
 }
 
+/**
+ * @brief Detect faces in the current frame obtained from the video stream.
+ * @return A vector of cv::Rect representing the detected faces' bounding boxes.
+ */
 std::vector<cv::Rect> DetectionClass::detectFaces() {
     // Function implementation
     // Process a frame from the video stream and perform face detection

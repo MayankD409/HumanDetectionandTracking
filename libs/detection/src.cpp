56ddd4bf13700e3fc38a4d6f23995f771f39e08a
Mayank Deshpande <deshpandemayank5@gmail.com>
2023-10-22 11:54:23 -0400

    Modified libs/detection/src.cpp
    Added empty function placeholders for the detection class.

--- libs/detection/src.cpp
+++ libs/detection/src.cpp
@@ -1,4 +1,22 @@
-int my_function1(int input)
-{
-  return input;
+#include "detection.hpp"
+
+DetectionClass::DetectionClass(const std::string& modelPath, const std::string& configPath) {
+    // Constructor implementation (if any)
+    // Initialize the face detection model with the provided paths
+}
+
+DetectionClass::~DetectionClass() {
+    // Destructor implementation (if any)
+}
+
+bool DetectionClass::initVideoStream(int deviceID) {
+    // Function implementation
+    // Initialize the video stream from the specified device (camera)
+    // Return true if successfully opened, false otherwise
+}
+
+std::vector<cv::Rect> DetectionClass::detectFaces() {
+    // Function implementation
+    // Process a frame from the video stream and perform face detection
+    // Return a vector of cv::Rect representing detected faces
 }

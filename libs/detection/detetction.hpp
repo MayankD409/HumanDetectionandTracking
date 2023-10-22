e0f18daf394f7cce9e33b858d9a25df27989d0bd
Mayank Deshpande <deshpandemayank5@gmail.com>
2023-10-22 11:08:01 -0400

    Class declaration for face detection Class.
    Modidfied detection.hpp to add the class declaration with functions
    and variables declaration.

--- libs/detection/detetction.hpp
+++ libs/detection/detetction.hpp
@@ -1,3 +1,19 @@
-#pragma once
+#ifndef DETECTION_HPP
+#define DETECTION_HPP
 
-int my_function1(int input);
+#include <opencv2/opencv.hpp>
+#include <iostream> // for input/output operations
+#include <vector>   // for using std::vector
+class DetectionClass {
+public:
+    DetectionClass(const std::string& modelPath, const std::string& configPath);
+    ~DetectionClass();
+    bool initVideoStream(int deviceID);  // Initialize video stream from camera
+    std::vector<cv::Rect> detectFaces(); // Detect faces in the current frame
+
+private:
+    cv::dnn::Net faceDetectionModel;
+    cv::VideoCapture videoCapture;
+};
+
+#endif // DETECTION_HPP

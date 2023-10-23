4fdafd7ee8576b378891d55f0b64f345491c7298
Mayank Deshpande <deshpandemayank5@gmail.com>
2023-10-23 19:48:53 -0400

    Added the DisplayClass skeleton
    Modified displaying.hpp and displaying.cpp with the declarations and
    also edited the CmakeLists.txt of both tracking and displaying libraries
    to add line to find the openCV package

--- libs/displaying/displaying.hpp
+++ libs/displaying/displaying.hpp
@@ -1,3 +1,49 @@
-#pragma once
+/**
+ * @file displaying.hpp
+ * @author Lowell Lobo
+ * @author Mayank Deshpande
+ * @author Kautilya Chappidi
+ * @brief Class Definition for the DisplayClass
+ * @version 0.1
+ * @date 2023-10-23
+ *
+ * @copyright Copyright (c) 2023
+ */
 
-float my_function2(float input);
+#ifndef DISPLAY_HPP
+#define DISPLAY_HPP
+
+#include <opencv2/core.hpp>
+#include <vector>
+#include <tuple>
+#include <string>
+
+/**
+ * @class DisplayClass
+ * @brief A class for displaying object locations and additional information on the video stream.
+ *
+ * This class provides functions to draw bounding boxes around detected objects
+ * and display additional information on top of the bounding boxes.
+ */
+class DisplayClass {
+public:
+    /**
+     * @brief Default constructor.
+     */
+    DisplayClass();
+
+    /**
+     * @brief Default destructor.
+     */
+    ~DisplayClass();
+
+    /**
+     * @brief Draws object locations and additional information on the video frame.
+     * @param frame The input video frame.
+     * @param objectLocations Vector of tuples containing object ID, bounding box
+     * @return Modified video frame with bounding boxes and object information.
+     */
+    cv::Mat drawObjectLocations(const cv::Mat& frame, const std::vector<std::tuple<int, cv::Rect>>& objectLocations);
+};
+
+#endif // DISPLAY_HPP

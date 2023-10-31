91ac0354fc6e70674715bdc5a24550d64f2b9f02
Mayank Deshpande <deshpandemayank5@gmail.com>
2023-10-31 17:07:53 -0400

    resolved app/main.cpp cpplint errors

--- app/main.cpp
+++ app/main.cpp
@@ -17,90 +17,103 @@
 #include "tracking.hpp"
 
 /**
- * @brief The main method of the file used to test and check whether the library works as intended
- * 
- * @return int 
+ * @brief The main method of the file used to test and check whether the library
+ * works as intended
+ *
+ * @return int
  */
-int main() { 
-    /**
-     * @brief variables used to get the natural configuration of camera and car
-     * 
-     */
-    double x, y, z, th, tv;
-    std::cout << "Enter x offset distance in inches: ";
-    std::cin >> x;
-    std::cout << std::endl << "Enter y offset distance in inches: ";
-    std::cin >> y;
-    std::cout << std::endl << "Enter z offset distance in inches: ";
-    std::cin >> z;
-    std::cout << std::endl << "Enter horizontal field of view angle in radians: ";
-    std::cin >> th;
-    std::cout << std::endl << "Enter vertical field of view angle in radians: ";
-    std::cin >> tv;
+int main() {
+  /**
+   * @brief variables used to get the natural configuration of camera and car
+   *
+   */
+  double x, y, z, th, tv;
+  std::cout << "Enter x offset distance in inches: ";
+  std::cin >> x;
+  std::cout << std::endl << "Enter y offset distance in inches: ";
+  std::cin >> y;
+  std::cout << std::endl << "Enter z offset distance in inches: ";
+  std::cin >> z;
+  std::cout << std::endl << "Enter horizontal field of view angle in radians: ";
+  std::cin >> th;
+  std::cout << std::endl << "Enter vertical field of view angle in radians: ";
+  std::cin >> tv;
+
+  /**
+   * @brief Initialise a tracker class to be used for tracking obstacles
+   *
+   */
+  TrackingClass tracker(
+      "../models/res10_300x300_ssd_iter_140000_fp16.caffemodel",
+      "../models/deploy.prototxt.txt", x, y, z, th, tv);
+
+  /**
+   * @brief Initialise the video
+   *
+   */
+  if (!tracker.image.initVideoStream(0)) {
+    return 0;
+  }
+
+  /**
+   * @brief Initialise a frame
+   *
+   */
+  cv::Mat frame;
+
+  while (true) {
+    tracker.image.videoCapture >> frame;
 
     /**
-     * @brief Initialise a tracker class to be used for tracking obstacles
-     * 
+     * @brief Get detections and store in variable rectangles
+     *
      */
-    TrackingClass tracker( "../models/res10_300x300_ssd_iter_140000_fp16.caffemodel", "../models/deploy.prototxt.txt", x, y, z, th, tv);
+    auto rectangles = tracker.image.detectFaces(frame);
 
     /**
-     * @brief Initialise the video 
-     * 
+     * @brief Assign IDs to the detections
+     *
      */
-    if (!tracker.image.initVideoStream(0)) {
-        return 0;
+    tracker.obstacleMapVector = tracker.assignIDAndTrack(rectangles);
+
+    /**
+     * @brief Find distance from camera frame
+     *
+     */
+    auto cameraDistance = tracker.distFromCamera(frame.cols, frame.rows);
+
+    /**
+     * @brief Find distance from car frame in inches
+     *
+     */
+    auto carDistance = tracker.distFromCar(cameraDistance);
+
+    cv::Scalar color(0, 105, 205);
+
+    /**
+     * @brief Draw rectangles for the detections and the distances
+     *
+     */
+    for (const auto& r : tracker.obstacleMapVector) {
+      cv::rectangle(frame, r.second, color, 4);
+      cv::putText(frame,
+                  std::to_string(r.first) + ": (" +
+                      std::to_string(
+                          static_cast<int>(std::get<0>(carDistance[r.first]))) +
+                      ", " +
+                      std::to_string(
+                          static_cast<int>(std::get<1>(carDistance[r.first]))) +
+                      ", " +
+                      std::to_string(
+                          static_cast<int>(std::get<2>(carDistance[r.first]))) +
+                      ")",
+                  cv::Point(r.second.x, r.second.y - 5),
+                  cv::FONT_HERSHEY_COMPLEX, 1.0, CV_RGB(255, 0, 0), 2);
     }
 
     /**
-     * @brief Initialise a frame
-     * 
-     */
-    cv::Mat frame;
-
-    while (true){
-        tracker.image.videoCapture >> frame;
-
-        /**
-         * @brief Get detections and store in variable rectangles
-         * 
-         */
-        auto rectangles = tracker.image.detectFaces(frame);
-
-        /**
-         * @brief Assign IDs to the detections
-         * 
-         */
-        tracker.obstacleMapVector = tracker.assignIDAndTrack(rectangles);
-
-        /**
-         * @brief Find distance from camera frame
-         * 
-         */
-        auto cameraDistance = tracker.distFromCamera(frame.cols, frame.rows);
-
-        /**
-         * @brief Find distance from car frame in inches
-         * 
-         */
-        auto carDistance = tracker.distFromCar(cameraDistance);
-
-        cv::Scalar color(0, 105, 205);
-
-        /**
-         * @brief Draw rectangles for the detections and the distances
-         * 
-         */
-        for(const auto & r : tracker.obstacleMapVector){
-            cv::rectangle(frame, r.second, color, 4);
-            cv::putText(frame, std::to_string(r.first) + ": (" + std::to_string((int)std::get<0>(carDistance[r.first])) + ", "+ std::to_string((int)std::get<1>(carDistance[r.first])) + ", "+ std::to_string((int)std::get<2>(carDistance[r.first])) + ")", cv::Point(r.second.x, r.second.y - 5),
-             cv::FONT_HERSHEY_COMPLEX,
-             1.0, CV_RGB(255, 0, 0), 2);
-        }
-        
-    /**
      * @brief Display the frame
-     * 
+     *
      */
     imshow("Image", frame);
     int esc_key = 27;
